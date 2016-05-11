02ea01f77e8cce9eb12d03a6515ae0c3adbf2293
mai.duc.thach <mai0214cs@gmail.com>
2016-05-11 14:06:44 +0700

    add controller

---
+++ blog/app/Http/Controllers/BaseController.php
@@ -0,0 +1,12 @@
+<?php
+namespace App\Http\Controllers;
+
+/**
+ * Description of BaseController
+ *
+ * @author BADBOY
+ */
+class BaseController extends Controller{
+    function __construct() {
+    }
+}
